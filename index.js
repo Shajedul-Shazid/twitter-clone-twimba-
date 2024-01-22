dcfc408e3e3072806bc6a57455e0423d93e911d8
Shajedul-Shazid <programmershajid@gmail.com>
2024-01-22 13:19:31 +0600

    retweets color added

--- index.js
+++ index.js
@@ -53,10 +53,17 @@ function getFeedHtml(){
     tweetsData.forEach(function(tweet){
 
         let likeIconClass = ''
+        let retweetIconClass = ''
 
         if(tweet.isLiked){
             likeIconClass = 'liked'
         }
+
+        if(tweet.isRetweeted){
+            retweetIconClass = 'retweeted'
+        }
+
+
 
 
         feedHtml += `
@@ -80,7 +87,7 @@ function getFeedHtml(){
                     ${tweet.likes}
                 </span>
                 <span class="tweet-detail">
-                    <i class="fa-solid fa-retweet"
+                    <i class="fa-solid fa-retweet ${retweetIconClass}"
                     data-retweet="${tweet.uuid}"
                     ></i>
                     ${tweet.retweets}
